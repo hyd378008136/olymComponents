7a868db6066fa002d214a42ee4fef82a365c2e3d
yidih <yidih@olymtech.com>
2017-12-26 15:10:39 +0800

    update

---
+++ samples/pages/components/toe.jsx
@@ -0,0 +1,18 @@
+import React,{Component} from "react";
+import {Toe} from 'olym'
+
+const value = "安达市大军大所大大大大大所大所安达市大军大所大大大大大"
+
+class ToeSample extends Component{
+    constructor(props) {
+        super(props)
+    }
+
+    render(){
+        return (
+            <Toe width="400px" value={value}/>
+        )
+    }
+}
+
+export default ToeSample

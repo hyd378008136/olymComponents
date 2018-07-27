44e99bc7e9d5b713bfc39e753506166675106a6e
王晓东 <wxd@olymtech.com>
2018-07-27 13:47:41 +0800

    textareadome

--- samples/pages/components/TextAreaDemo.jsx
+++ samples/pages/components/TextAreaDemo.jsx
@@ -1,26 +1,49 @@
 import React, {Component} from 'react'
 import TextArea from '../../../src/components/olym/input-textarea'
-import {Form, Button} from 'antd'
+import TextAreaSelect from '../../../src/components/antd/text-area-select'
+import {Form, Button, Select} from 'antd'
 import {FormLayout} from 'olym'
 const FormItem = FormLayout.FormItem
+const Option = Select.Option;
+let id = 0;
+let _selectTimeOut = null;
 
 class TextAreaDemo extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { value: '' };
+		this.state = { value: '', dataList: [] };
 	}
 	getValue = () => {
 		this.props.form.validateFieldsAndScroll((errors, values) => {
 			console.log(values)
 		})
 	}
+	getPopupContainer = () => document.getElementById('pop-up')
+	handleSearch = (value) => {
+		if (_selectTimeOut) {
+			clearTimeout(_selectTimeOut)
+			_selectTimeOut = null
+		}
+		const fake = () => {
+			let _dataList = []
+			for (let i = 0; i < 10; i++){
+				_dataList.push('test'+ i + id)
+			}
+			id++
+			this.setState({
+				dataList: _dataList
+			})
+		}
+		_selectTimeOut = setTimeout(fake, 300);
+	}
 	render() {
 		const {getFieldDecorator} = this.props.form
 		return (
-			<div>
+			<FormLayout>
+				<div id="pop-up"> </div>
 				<Button onClick = {this.getValue}>取值</Button>
 
-				<FormItem helpPosition={"right"} label = '输入框 demo'>
+				<FormItem helpPosition={"right"} label = '输入框 demo' labelWidth="8em">
 					{getFieldDecorator('TextAreaDemo', {
 						rules: [{
 							required: true,
@@ -29,7 +52,37 @@ class TextAreaDemo extends Component {
 						}]
 					})(<TextArea autosize={{minRows: 5, maxRows: 6}}/>)}
 				</FormItem>
-			</div>
+				<FormItem helpPosition={"bottom"} label = 'textareaselect' labelWidth="8em">
+					{getFieldDecorator('TextAreaSelectDemo', {
+						rules: [{
+							required: true,
+							max: 9,
+							message:'最长9'
+						}]
+					})(
+						<TextAreaSelect
+							mode="combobox"
+							allowclear
+							style={{ width: 200 }}
+							placeholder="Select a person"
+							// optionFilterProp="children"
+							// onChange={handleChange}
+							// onFocus={handleFocus}
+							// onBlur={handleBlur}
+							showSearch={true}
+							onFirstFocus={this.handleSearch}
+							onSearch={this.handleSearch}
+							getPopupContainer={this.getPopupContainer}
+							filterOption={false}
+							// filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+						>
+							{
+								this.state.dataList.map((item) => <Option ket={item} value={item}>{item}</Option>)
+							}
+						</TextAreaSelect>
+					)}
+				</FormItem>
+			</FormLayout>
 		)
 	}
 }
